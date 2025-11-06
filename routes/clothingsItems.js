c3eebde18f5bb51232df9d000bd23a3fb6e69ea7
Damian <dcamp090799@gmail.com>
2025-11-05 20:01:42 -0500

    feat: enhance delete item route with ID validation

--- routes/clothingsItems.js
+++ routes/clothingsItems.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
 const {
   getItems,
   createItem,
@@ -7,6 +8,14 @@ const {
 
 router.get("/", getItems);
 router.post("/", createItem);
-router.delete("/:itemId", deleteItem);
+router.delete("/:itemId", (req, res, next) => {
+  const { itemId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
+    return res.status(400).send({ message: "Invalid item ID" });
+  }
+
+  return deleteItem(req, res, next);
+});
 
 module.exports = router;
