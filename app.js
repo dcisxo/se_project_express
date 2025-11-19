6ed86a2c61bd7a47673770ca9b23d09c1ac1d8e7
Damian <dcamp090799@gmail.com>
2025-11-19 14:37:52 -0500

    refactor: move authentication middleware to apply globally

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const usersRouter = require("./routes/users");
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
-app.use(auth);
 
 // Public routes (no auth required)
 app.post("/signin", login);
