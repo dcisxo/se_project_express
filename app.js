eccab52244be7d2083a783728f98bf2d301674d8
Damian <dcamp090799@gmail.com>
2025-11-24 09:54:03 -0500

    feat: add temporary middleware for user authorization during testing

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(cors());
 app.use(express.json());
 
+// Temporary authorization solution for testing
+app.use((req, res, next) => {
+  req.user = {
+    _id: "690e73961b205cacc1369140",
+  };
+  next();
+});
+
 // Public routes (no auth required)
 app.post("/signin", login);
 app.post("/signup", createUser);
