85b357d335adfcaf2124eceac8b8c4749620018a
Damian <dcamp090799@gmail.com>
2026-02-18 20:14:49 -0500

    refactor: update user schema to make name and avatar fields optional and improve error messages

--- models/users.js
+++ models/users.js
@@ -5,13 +5,14 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: "Shyann Koch",
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
     validate: {
       validator: (v) => validator.isURL(v),
       message: "You must enter a valid URL",
@@ -29,7 +30,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8,
     select: false,
   },
 });
@@ -42,11 +42,11 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
     .select("+password")
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error("Invalid email or password"));
+        return Promise.reject(new Error("Incorrect email or password"));
       }
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          return Promise.reject(new Error("Invalid email or password"));
+          return Promise.reject(new Error("Incorrect email or password"));
         }
         return user;
       });
